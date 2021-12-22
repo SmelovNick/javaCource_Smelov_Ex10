7969ee171cf0b69c730c46f0d6bcd815753375bb
nikolai.smelov <yarosmilk@gmail.com>
2021-12-22 16:48:00 +0300

    Added solution for ex10

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try{
-            Scanner scr = new Scanner(System.in);
-            System.out.print("Введите строку, содержащую одну цифру: ");
-            char[] arrayCharWithDigit = scr.nextLine().toCharArray();
-            for (char character: arrayCharWithDigit) {
-                if (Character.isDigit(character)) throw new DigitException("Строка содержит цифру", Integer.parseInt(Character.toString(character)));
+        boolean flag = true;
+        while (flag) {
+            try {
+                Scanner scr = new Scanner(System.in);
+                System.out.print("Введите строку, не содержащую одну цифру: ");
+                char[] arrayCharWithDigit = scr.nextLine().toCharArray();
+                for (char character : arrayCharWithDigit) {
+                    if (Character.isDigit(character))
+                        throw new DigitException("Строка содержит цифру", Integer.parseInt(Character.toString(character)));
+                }
+                flag = false;
+            } catch (DigitException de) {
+                System.out.println(de.getMessage() + " " + de.getNumber());
             }
-        }catch (DigitException de){
-            System.out.print(de.getMessage()+ " " + de.getNumber());
         }
     }
 }
